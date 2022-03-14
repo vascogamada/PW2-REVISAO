814dba76230a09e2dee59818d9498ee10d5b7bc5
ViniMateos-GabrielLima <vini-gabriel@test.com>
2022-03-14 10:49:08 -0300

    Exercicios de 14/03/2022

---
+++ src/ExercicioUm.java
@@ -0,0 +1,13 @@
+
+public class ExercicioUm 
+{
+	public static void main(String[] args) 
+	{
+		int idade = 20;
+		String nome = "Teresvaldo", sobrenome ="Gersonildo I";
+		String resultado = nome + " " + sobrenome + " tem " + idade + " anos.";
+		System.out.println(resultado);
+		
+		
+	}
+}
---
+++ src/PimeiroExemplo.java
@@ -0,0 +1,11 @@
+
+public class PimeiroExemplo 
+{
+	public static void main(String[] args) 
+	{
+		int idade = 20;
+		String nome = "Lima";
+		System.out.println("Seu nome é: " +  nome);
+		System.out.println("Sua idade é: " + idade);
+	}
+}
