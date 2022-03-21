44e5c1a09485e128b531a2ba4510eaac4d96c7c7
nome1-nome2 <nome1-nome2@test.com>
2022-03-21 10:49:04 -0300

    Desenvolvimento do Exercicio 3

---
+++ src/ExercicioTres.java
@@ -0,0 +1,23 @@
+
+public class ExercicioTres 
+{
+	public static void main(String[] args) 
+	{
+		int numero = 0;
+		
+		if (numero > 0) 
+		{
+			System.out.println("1");
+		}
+		
+		else if (numero < 0)
+		{
+			System.out.println("-1");
+		}
+		else if (numero < 1 && numero > -1)
+		{
+			System.out.println("0");
+		}
+
+	}
+}
