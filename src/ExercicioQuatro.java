4d44c90ae1d607bd4295c9f4beb6b18592d664a9
nome1-nome2 <nome1-nome2@test.com>
2022-03-21 10:59:27 -0300

    Desenvolvimento do exercicio 4

---
+++ src/ExercicioQuatro.java
@@ -0,0 +1,18 @@
+
+public class ExercicioQuatro 
+{
+	public static void main(String[] args) 
+	{
+		int numero = 14;
+		
+		if (numero >1 && numero % 3 == 0 || numero % 2 == 0 && numero != 15)
+		{
+			System.out.println("Esse é um número da sorte");
+		}
+		
+		else
+		{
+			System.out.println("Esse não é um número da sorte");
+		}
+	}
+}
