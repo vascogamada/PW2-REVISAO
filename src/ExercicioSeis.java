ef20bb9a6fc2742b87785738d9c0eef76693c980
ViniciusMateos-Gabriel <ViniciusMateos-GabrielLima@test.com>
2022-03-28 10:18:22 -0300

    Desenvolvimento do exercicio 6

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,23 @@
+
+public class ExercicioSeis 
+{
+	public static void main(String[] args) 
+	{
+		double peso =9;
+		
+		if (peso < 4)
+		{
+			System.out.println("Está abaixo do peso.");
+		}
+		
+		else if (peso > 10)
+		{
+			System.out.println("Está acima do peso.");
+		}
+		
+		else if (peso >= 4 && peso <=10)
+		{
+			System.out.println("Está no peso ideal.");
+		}
+	}
+}
