9b96445cf532c42986daee0f18cf8a12583a1e94
ViniciusMateos-Gabriel <ViniciusMateos-GabrielLima@test.com>
2022-03-28 10:11:16 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,12 @@
+
+public class ExercicioCinco 
+{	
+	public static void main(String[] args) 
+	{
+		double resultado, historia = 8.0, matematica = 7.0, ciencia = 10, geografia = 9.5; 
+		
+		resultado = (matematica + geografia + historia + ciencia) / 4;
+		
+		System.out.printf("A média é: %.1f", resultado);
+	}
+}
